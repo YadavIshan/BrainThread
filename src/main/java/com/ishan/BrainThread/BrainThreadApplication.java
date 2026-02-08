e6a2018cadae40673c3ce919e53da19d06428ee3
Ishan Yadav <37396882+YadavIshan@users.noreply.github.com>
2026-02-08 16:38:43 +0530

    Initial commit

---
+++ src/main/java/com/ishan/BrainThread/BrainThreadApplication.java
@@ -0,0 +1,13 @@
+package com.ishan.BrainThread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainThreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainThreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ishan/BrainThread/BrainThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishan.BrainThread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
